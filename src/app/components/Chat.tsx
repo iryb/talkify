f3b60629d6ee72b35a072bfd0ecc07146d1eebd8
Iryna <iryna.burianovata@gmail.com>
2024-06-27 13:06:58 +0300

    Display transcription

--- src/app/components/Chat.tsx
+++ src/app/components/Chat.tsx
@@ -1,81 +1,87 @@
-"use client"
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
-import React from 'react'
+"use client";
+import React, { useState } from "react";
 
 export const Chat = () => {
-    const {
-        transcript,
-        listening,
-        resetTranscript,
-        browserSupportsSpeechRecognition
-      } = useSpeechRecognition();
+  const listening = false;
+  const [transcript, setTranscript] = useState<string>();
 
-    if (!browserSupportsSpeechRecognition) {
-        return <span>Browser doesn't support speech recognition.</span>;
-    }
+  // const { mutate: sendMessage, isPending: isLoading } = useMutation({
+  //     mutationFn: async (message: Message) => {
+  //       const response = await fetch("/api/message", {
+  //         method: "POST",
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //         },
+  //         body: JSON.stringify({ messages: [message] }),
+  //       });
 
-    const { mutate: sendMessage, isPending: isLoading } = useMutation({
-        mutationFn: async (message: Message) => {
-          const response = await fetch("/api/message", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ messages: [message] }),
-          });
-    
-          if (!response.ok) {
-            throw new Error();
-          }
-    
-          return response.body;
-        },
-        onMutate(message) {
-          addMessage(message);
-        },
-        onSuccess: async (stream) => {
-          if (!stream) throw new Error("No stream found");
-    
-          const id = nanoid();
-          const responseMessage: Message = {
-            id,
-            isUserMessage: false,
-            text: "",
-          };
-    
-          addMessage(responseMessage);
-    
-          setIsMessageUpdating(true);
-    
-          const reader = stream.getReader();
-          const decoder = new TextDecoder();
-          let done = false;
-    
-          while (!done) {
-            const { value, done: doneReading } = await reader.read();
-            done = doneReading;
-            const chunkValue = decoder.decode(value);
-            updateMessage(id, (prev) => prev + chunkValue);
-          }
-    
-          setIsMessageUpdating(false);
-          setInput("");
-          setTimeout(() => textareaRef.current?.focus(), 10);
-        },
-        onError(_, message) {
-          toast.error("Something went wrong. Please try again.");
-          removeMessage(message.id);
-          textareaRef.current?.focus();
-        },
-      });
+  //       if (!response.ok) {
+  //         throw new Error();
+  //       }
 
-    return (
+  //       return response.body;
+  //     },
+  //     onMutate(message) {
+  //       addMessage(message);
+  //     },
+  //     onSuccess: async (stream) => {
+  //       if (!stream) throw new Error("No stream found");
+
+  //       const id = nanoid();
+  //       const responseMessage: Message = {
+  //         id,
+  //         isUserMessage: false,
+  //         text: "",
+  //       };
+
+  //       addMessage(responseMessage);
+
+  //       setIsMessageUpdating(true);
+
+  //       const reader = stream.getReader();
+  //       const decoder = new TextDecoder();
+  //       let done = false;
+
+  //       while (!done) {
+  //         const { value, done: doneReading } = await reader.read();
+  //         done = doneReading;
+  //         const chunkValue = decoder.decode(value);
+  //         updateMessage(id, (prev) => prev + chunkValue);
+  //       }
+
+  //       setIsMessageUpdating(false);
+  //       setInput("");
+  //       setTimeout(() => textareaRef.current?.focus(), 10);
+  //     },
+  //     onError(_, message) {
+  //       toast.error("Something went wrong. Please try again.");
+  //       removeMessage(message.id);
+  //       textareaRef.current?.focus();
+  //     },
+  //   });
+
+  const handleStart = () => {
+    const SpeechRecognition =
+      window.SpeechRecognition || window.webkitSpeechRecognition;
+    const recognition = new SpeechRecognition();
+
+    recognition.onresult = function (event) {
+      const input = event.results[0][0].transcript;
+      setTranscript(input);
+      console.log("Confidence: " + event.results[0][0].confidence);
+    };
+
+    recognition.start();
+  };
+
+  const handleStop = () => {};
+
+  return (
     <div>
-        <p>Microphone: {listening ? 'on' : 'off'}</p>
-        <button onClick={() => SpeechRecognition.startListening}>Start</button>
-        <button onClick={SpeechRecognition.stopListening}>Stop</button>
-        <button onClick={resetTranscript}>Reset</button>
-        <p>{transcript}</p>
+      <p>Microphone: {listening ? "on" : "off"}</p>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
+      <p>{transcript}</p>
     </div>
-    );
-}
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
-"use client"
-import 'regenerator-runtime/runtime'
-import Dictaphone from "@/app/components/Dictaphone"
+"use client";
+import "regenerator-runtime/runtime";
+import { Chat } from "@/app/components/Chat";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
-    <Dictaphone />
-
+      <Chat />
     </main>
   );
 }
