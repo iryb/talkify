c496275b0ff5c21429559a12012d73b9b1e1db0c
iryb <iryna.burianovata@gmail.com>
2024-07-05 16:55:16 +0300

    Add Chat page

---
+++ src/app/add-chat/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+import { AddChat } from "../components/AddChat";
+
+const Page = () => {
+  return (
+    <div>
+      <AddChat />
+    </div>
+  );
+};
+
+export default Page;
