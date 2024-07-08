4e575d5b85eeaa8ccc5c42b1e12be1bd877f0512
Iryna <iryna.burianovata@gmail.com>
2024-07-08 18:12:21 +0300

    Add layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { MessagesProvider } from "@/context/messages";
+import { Header } from "@/components/Header";
+import { Sidebar } from "@/components/Sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +15,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <MessagesProvider>
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <Header />
+          <div className="flex h-[calc(100vh-4rem)]">
+            <Sidebar />
+            <div className="w-3/4">{children}</div>
+          </div>
+        </body>
       </MessagesProvider>
     </html>
   );
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <header className="border-b bg-slate-200 border-slate-300 h-16">
+      <div className="px-4 py-4">
+        <div className="text-xl uppercase font-bold tracking-wider text-slate-800">
+          Talkify
+        </div>
+      </div>
+    </header>
+  );
+};
