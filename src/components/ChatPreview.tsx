8dd673b734d249e9cfe825666efffe1db121185c
iryb <iryna.burianovata@gmail.com>
2024-07-19 19:03:37 +0300

    Remove chat history when chat is closed

--- src/components/ChatPreview.tsx
+++ src/components/ChatPreview.tsx
@@ -1,4 +1,5 @@
 import { useChats } from "@/context/chats";
+import { useMessages } from "@/context/messages";
 import { deleteChat } from "@/firebase/chat/chat";
 import { Chat } from "@/lib/validators/chat";
 import clsx from "clsx";
@@ -18,11 +19,26 @@ export const ChatPreview = ({
   className,
 }: ChatPreviewProps) => {
   const { chats, setActiveChat, removeChat } = useChats();
+  const { messages, removeAllMessages } = useMessages();
 
   const handleDeleteChat = async (id: string) => {
     const deletedChatId = await deleteChat(id);
     removeChat(deletedChatId);
     setActiveChat(chats[0].id);
+  };
+
+  const handleChangeChat = (id: string) => {
+    if (messages.length > 1) {
+      const closeChat = confirm(
+        "Do you want to close current chat? Chat history will be removed."
+      );
+      if (closeChat) {
+        removeAllMessages();
+        setActiveChat(id);
+      }
+    } else {
+      setActiveChat(id);
+    }
   };
 
   return (
@@ -31,7 +47,7 @@ export const ChatPreview = ({
         "relative group p-2 hover:bg-slate-300 hover:cursor-pointer transition-all",
         className
       )}
-      onClick={() => setActiveChat(id)}
+      onClick={() => handleChangeChat(id)}
     >
       <a
         className="absolute top-2 right-2 hidden group-hover:block hover:text-white"
