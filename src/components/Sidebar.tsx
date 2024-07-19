59b639803ebecf2e9b0fce4d5695f9c274b0c049
iryb <iryna.burianovata@gmail.com>
2024-07-19 18:06:04 +0300

    Sort chats by date

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,9 +3,11 @@ import { ChatPreview } from "./ChatPreview";
 import Link from "next/link";
 import { Plus } from "lucide-react";
 import { useChats } from "@/context/chats";
+import { sortChatsByDate } from "@/lib/utils";
 
 export const Sidebar = () => {
   const { chats } = useChats();
+  const sortedChats = sortChatsByDate(chats);
 
   return (
     <div className="relative bg-slate-100 w-1/4 border-r border-slate-300 px-4 py-8">
@@ -17,10 +19,21 @@ export const Sidebar = () => {
       >
         <Plus />
       </Link>
-      {chats.length > 0 && (
+      {sortedChats.length > 0 && (
         <div>
-          {chats.map(
-            ({ id, lessonTopic, grammarTopic, vocabulary, level }, index) => (
+          {sortedChats.map(
+            (
+              {
+                id,
+                lessonTopic,
+                grammarTopic,
+                vocabulary,
+                level,
+                createdAt,
+                modifiedAt,
+              },
+              index
+            ) => (
               <ChatPreview
                 key={index}
                 lessonTopic={lessonTopic}
@@ -31,6 +44,8 @@ export const Sidebar = () => {
                   index === chats.length - 1 ? "" : "border-b border-slate-300"
                 }
                 level={level}
+                createdAt={createdAt}
+                modifiedAt={modifiedAt}
               />
             )
           )}
